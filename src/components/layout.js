f379f4230c1be0d5d2bc5b9ff8b908ec6c308e5b
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2022-01-30 16:32:35 +0530

    Essential svgs added and html for footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Navbar from './navbar.js';
+import Footer from "./footer.js";
 import "./layout.css"
 export default function Layout({ children }) {
-  return <div>{children}</div>
+  return  (
+    <div>{children}
+        <Navbar/>
+        <Footer/>
+    </div>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,12 @@
 import * as React from "react"
-import App from "./App"
+
 import Layout from "../components/layout"
 // markup
 const IndexPage = () => {
   return (
    <Layout>
-     <App/>
-   </Layout>
-     
-        
-      
+    
+   </Layout> 
   )
 }
 
