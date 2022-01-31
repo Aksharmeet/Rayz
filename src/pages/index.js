6829bb633e8da5df56e116783f0932f0b75efb93
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2022-01-31 23:44:25 +0530

    created index.js and responsive.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,29 @@
 import * as React from "react"
 import Footer from "../components/footer";
 import Navbar from '../components/navbar';
+import styled from "styled-components";
 import '../components/layout.css'
-
+import Home from "../components/Home"
 // markup
 const IndexPage = () => {
   return (
     <div>
       <Navbar/>
-       
+      <Main>
+            <Slider/>
+            
+      </Main>
       <Footer/>
     </div>
   )
 }
+const Main = styled.section`
+  background-image: url('./assets/main_bg.svg');
+  background-size:cover;
+  background-repeat:no-repeat;
+  background-position:center;
+
+  padding:100px 20px 0 20px;
+`
 
 export default IndexPage
