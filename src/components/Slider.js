4cc1d49dbb9d50856693ba4a5ee863dc5d1c2817
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2022-02-04 23:15:22 +0530

    main changes in slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -5,21 +5,10 @@ import styled from "styled-components"
 import arrow from "../assets/arrow.svg";
 import circle from "../assets/Circle.svg";
 function Slider() {
-  const [index,setIndex] = useState(0);
+  const [count,setIndex] = useState(0);
   
-  
-  
-  const increment = () => {
-    setIndex((prevIndex) =>
-    prevIndex === 2 ? 0 : prevIndex + 1
-    );
-   
-  }
-  const decrement = () => {
-    setIndex((prevIndex) =>
-    prevIndex === 0 ? 2 : prevIndex - 1
-  );
-  }
+
+  console.log(count);
 return (
   <StaticQuery
     query={graphql`
@@ -43,27 +32,29 @@ return (
     `}
     render={data => 
       <MainContainer>
-      <SlideShow>
-        <Sliding   style={{ transform: `translate3d(${-index * 100}%, 0, 0)`}}>
+        <Sliding style={{ transform: `translate3d(${-count * 100}vw, 0, 0)`}}>
           {data.allContentfulHome.edges.map(({node, index}) =>
-            <Slide className={`${node.title.toLowerCase()}_container`} key={index} >
-              <div className={`${node.title.toLowerCase()}_text`}>
-              <h1>{node.title}</h1>
-              <p>{node.description.description}</p>
-              </div>
+            <Slide className={`${node.title.toLowerCase().replace(/\s/g, "")}_container`} key={index} count={count} style={{ right:`${index * 100}vw`}} >
               <div className="emptyDiv"></div>
+              <div className="flex">
+              <div className='text_container'>
+              <h1>{node.title}</h1>
+              <h1>{node.id}</h1>
+              <p>{node.description.description}</p>
+              <Explore>
+            <img className={`${node.title.toLowerCase().replace(/\s/g, "")}_explore`} src={circle} alt="explore button"/>
+            </Explore>
+              </div>
               <img className={`${node.title.toLowerCase()}_img`} src={node.image.file.url}></img>
+              </div>
+             
             </Slide>
           )}
         </Sliding>
-     </SlideShow>
      <ArrowExplore>
-          <Explore>
-            <img src={circle} alt="explore button"/>
-          </Explore>
           <Arrows>
-            <img alt="left arrow" className="l_arrow"src={arrow} onClick={increment}/>
-            <img alt="right arrow"  className="r_arrow"src={arrow} onClick={decrement} />
+            <img alt="left arrow" className="l_arrow"src={arrow} onClick={() => setIndex((prevIndex) =>prevIndex === 0 ? 2 : prevIndex - 1)}/>
+            <img alt="right arrow"  className="r_arrow"src={arrow} onClick={() => setIndex((prevIndex) =>prevIndex === 2 ? 0 : prevIndex + 1)} />
           </Arrows>
         </ArrowExplore>
      </MainContainer>
@@ -71,135 +62,172 @@ return (
   ></StaticQuery>
 )
 }
-const MainContainer = styled.section`
-  width:100%;
-  display:flex;
-  flex-direction:column;
-  
-  .chandelier_text{
-    margin-bottom:-100px;
-  }
-  @media(min-width:700px){
-    align-items:left;
-   
-    }
-`
-
-const SlideShow = styled.div`
-  margin: 0 auto;
-  @media(max-width:700px){
-    margin-top:-3vw;
-  }
-  
+const MainContainer = styled.div`
+height:92vh;
+overflow:hidden;
 `
 const Sliding = styled.div`
-  display:flex;
-  transition:all 1s;
-  align-items:top;
-  text-align:center;
-  width:100vw;
-  
+display:flex;
+.chandelier_container{
+  left:100vw;
   img{
-  width:80vw;
-  };
-  @media(min-width:700px){
+    margin-top:-70px
+  }
+}
+.floorlamps_container{
+  left:200vw;
+}
+.tablelamp_container{
+  
+}
+transition:all .5s;
+@media(min-width:1100px){
+  .chandelier_container{
+    top:-100px;
     img{
-      width:60vw;
+      margin-top:0;
+    }
+  }
+    .tablelamp_container{
+      .text_container{
+        margin-top:-50px;
       }
-      height:60vh;
-  }
-  h1{
-    font-family:copper_light;
-    color:#f5eee4;
-  }
-  p{
-    width:70vw;
-    max-width:500px;
-    color:#f5eee4;
-  }
-
+    }
+  
+}
 `
 const Slide = styled.div`
-display:flex;
-align-items:center;
-flex-direction:column;
-width:100vw;
-.emptyDiv{
-  width:100vw;
-}
-@media(min-width:700px){
-  flex-direction:row;
-}
-
-
+  position:absolute;
+  text-align:center;
+  left:0;
+  right:0;
+  width:100%;
+  h1{
+    font-family:copper_light;
+    font-size:24px;
+  }
+  p{
+    font-family:lucida;
+    line-height:22px;
+    max-width:500px;
+    margin:20px auto 20px auto;
+    font-size:14px;
+  }
+  .flex{
+    display:flex;
+    align-items:center;
+    flex-direction:column;
+  }
+  img{
+    width:70%;
+    max-width:400px;
+  }
+  @media (min-width:800px){
+    h1{
+      font-size:28px;
+    }
+    p{
+      line-height:24px;
+      
+      font-size:16px;
+    }
+  }
+  @media (min-width:1100px){
+    text-align:left;
+    width:100%;
+    .flex{
+      width:100%;
+      flex-direction:row;
+      justify-content:space-between;
+    }
+    .text_container{
+      margin-left:20px;
+    }
+    img{
+      width:50%;
+      max-width:none;
+    }
+    h1{
+      font-size:32px;
+    }
+    p{
+      line-height:26px;
+      
+      font-size:17px;
+    }
+  }
+  @media (min-width:1400px){
+    max-width:1400px;
+    margin:0 auto;
+  }
  
 `
 const ArrowExplore = styled.div`
-
-
+  width:100%;
 `
 const Explore = styled.div`
+  position:absolute;
   width:100%;
-  display:flex;
-  justify-content:center;
-  margin-bottom:50px;
-  @media(max-width:700px){
-    margin-top:-100px;
-    }
   img{
-    width:20vw;
-    opacity:.6;
-    transition:opacity .3s;
+    width:25%;
     max-width:150px;
   }
-  img:hover{
-    opacity:.8;
-  }
-  @media(min-width:700px){
-    position:relative;
-    top:-60px;
-  }
-  @media(min-width:700px){
-    display:block;
-    margin-left:60px;
-    position:relative;
-    top:-100px;
-    margin-bottom:50px;
-  }
-  @media(min-width:1100px){
-    top:-175px;
-  }
-`
-const Arrows = styled.div`
-  display:flex;
-  flex-direction:column;
-  gap:5vw;
-  margin-bottom:32px;
-  position:absolute;
-  top:25vw;
-  right:20px;
-  @media(max-width:900px){
-    top:400px;
-   
-  }
-  .r_arrow{
-    transform:rotate(180deg);
-  }
-  .l_arrow, .r_arrow{
-    width:3vw;
-    opacity:.4;
-    transition:opacity .3s;
-  }
-  .l_arrow:hover, .r_arrow:hover{
-    opacity:.7;
-  }
-  @media(min-width:900px){
-    width:5vw;
-    .l_arrow, .r_arrow{
-      width:2vw;
+  top:65vh;
+  margin:0 auto;
+  left:0;
+  right:0;
+  @media (min-width:1100px){
+    top:40vh;
+    left:20px;
+  
+    .chandelier_explore{
+      position:absolute;
+      top:10vw;
+    }
+    .tablelamp_explore{
+      position:absolute;
+      top:5vw;
+    }
+    .floorlamps_explore{
+      position:absolute;
+      top:5vw;
+    }
+    @media (min-width:1300px){
+      .chandelier_explore{
+        position:absolute;
+        top:15vw;
+      }
     }
   }
  
+ 
 `
-
+const Arrows = styled.div`
+  position:absolute;
+  top:45vh;
+  right:20px;
+  height:20px;
+  display:flex;
+  flex-direction:column;
+  gap:5vh;
+  img{
+    cursor:pointer;
+    width:3vh;
+    max-width:80px;
+    opacity:.7;
+    transition:all .3s;
+  }
+  img:hover{
+    opacity:1;
+  }
+  .l_arrow{
+    transform:rotate(180deg);
+  }
+  @media (min-width:1100px){
+    right:50px;
+  }
+  img{
+    max-width:120px;
+  }
+`
 export default Slider
